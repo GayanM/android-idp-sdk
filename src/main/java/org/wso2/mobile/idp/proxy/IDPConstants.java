2c31648d4a09d99ed698318e7c012c48fd6747d6
gayan <gayan@gayanpc.(none)>
2014-04-15 00:27:23 +0530

    add src

---
+++ src/main/java/org/wso2/mobile/idp/proxy/IDPConstants.java
@@ -0,0 +1,10 @@
+package org.wso2.mobile.idp.proxy;
+
+/**
+ * package names of IDP proxy application
+ */
+public class IDPConstants {
+    final static String idpProxyPackage = "org.wso2.mobile.idp";
+    final static String idpProxyActivity = "org.wso2.mobile.idp.WebViewActivity";
+    final static String CALL_BACK_URL = "http://wso2.com";
+}
---
+++ src/main/java/org/wso2/mobile/idp/proxy/IdentityProxyActivity.java
@@ -0,0 +1,65 @@
+package org.wso2.mobile.idp.proxy;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+
+/**
+ * Encapsulate IDP proxy application information inside proxy
+ */
+public class IdentityProxyActivity extends Activity {
+    private static String TAG = "IdentityProxyActivity";
+    private static String clientID = null;
+    private static String clientSecret = null;
+
+    static void setClientCrdentials(String clientId, String client_Secret) {
+        clientID = clientId;
+        clientSecret = client_Secret;
+    }
+
+    public void sendResponse() {
+        Log.d("sendResponse", "sendResponse");
+        Intent intent = new Intent();
+       /* intent.putExtra("status",status);
+        intent.putExtra("message",message);*/
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        IdentityProxy.getInstance().setIdentityProxyActivity(this);
+        Log.d(TAG, "starting IdentityProxyActivity Activity");
+        final Intent loginIntent = new Intent("android.intent.action.MAIN");
+        loginIntent.setComponent(ComponentName.unflattenFromString(IDPConstants.idpProxyPackage + "/" + IDPConstants.idpProxyActivity));
+        IdentityProxy clientCredentials = IdentityProxy.getInstance();
+        Log.d(TAG, clientID);
+        loginIntent.putExtra("client_id", clientID);
+        startActivityForResult(loginIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "starting IdentityProxyActivity onActivityResult");
+        if (data != null) {
+            String code = data.getStringExtra("code");
+            String accessTokenURL = data.getStringExtra("access_token_url");
+            Log.v("Test Data", code);
+            TokenEndPoints tokenEndPoints = TokenEndPoints.getInstance();
+            tokenEndPoints.setAccessTokenURL(accessTokenURL);
+            super.onActivityResult(requestCode, resultCode, data);
+            try {
+                AccessTokenHandler accessTokenHandler = new AccessTokenHandler(getApplicationContext(), clientID, clientSecret);
+                accessTokenHandler.obtainAccessToken(code);
+            } catch (Exception e) {
+                Log.d("ERROR", e.toString());
+            }
+        }
+
+    }
+}
