ff5f97b966379a76102f7be288841b3eb88e0acc
gayan <gayan@gayanpc.(none)>
2014-05-15 13:08:57 +0530

    add samples and idp proxy app

---
+++ samples/azone/src/com/example/azone/OauthCostants.java
@@ -0,0 +1,8 @@
+package com.example.azone;
+
+public class OauthCostants {
+	final static String REDIRECT_URL = "http://wso2.com";
+	final static String CLIENT_ID = "x79Ko4kLiSuPG9uDE4evEeJ6UMca";
+	final static String CLIENT_SECRET = "F1tvBgguv0RCXr0f_aetP9SFzQsa";
+	final static String TRUSTSTORE_PASSWORD = "wso2carbon";
+}
